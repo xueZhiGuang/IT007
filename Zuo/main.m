785d37af8773648a03f263a52c09bd1802c72786
xueZhiGuang <mac@zuoxuefeng.local>
2016-06-24 15:34:30 -0700

    zuo
    新建的工程

---
+++ Zuo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zuo
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 ZXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
